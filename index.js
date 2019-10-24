7d892d5898e025a55a3504833bbc9aa2830e8438
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-10-23 21:04:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,106 +4,115 @@ const server = express();
 
 server.use(express.json());
 
-/**
- * A variável `projects` pode ser `const` porque um `array`
- * pode receber adições ou exclusões mesmo sendo uma constante.
- */
 const projects = [];
 
-/**
- * Middleware que checa se o projeto existe
- */
-function checkProjectExists(req, res, next) {
-  const { id } = req.params;
-  const project = projects.find(p => p.id == id);
+/* MIDDLEWARE
+Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto
+nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não 
+existir retorne um erro, caso contrário permita a requisição continuar normalmente;
+*/
+function checkID(req,res,next){
+  const {id} = req.params.id;
+  var project;
 
-  if (!project) {
-    return res.status(400).json({ error: 'Project not found' });
+  for (var i = 0; i < projects.length; i++){
+    if (projects[i].id == id){
+      project = projects[i];
+    }
   }
 
+  if (!project){
+    return res.status(400).json({error: 'id não existe'})
+  }
   return next();
 }
 
-/**
- * Middleware que dá log no número de requisições
- */
-function logRequests(req, res, next) {
-
-  console.count("Número de requisições");
-
+/* MIDDLEWARE
+Crie um middleware global chamado em todas requisições que imprime (console.log)
+uma contagem de quantas requisições foram feitas na aplicação até então;
+*/
+function contReq(req,res,next){
+  console.count();
   return next();
 }
 
-server.use(logRequests);
-
-/**
- * Retorna todos os projetos
- */
-server.get('/projects', (req, res) => {
-  return res.json(projects);
-});
-
-/**
- * Request body: id, title
- * Cadastra um novo projeto
- */
-server.post('/projects', (req, res) => {
-  const { id, title } = req.body;
-
+/* ROTA PARA ADICIONAR UM PROJETO
+POST /projects: A rota deve receber id e title dentro do corpo e cadastrar um 
+novo projeto dentro de um array no seguinte formato: 
+{ id: "1", title: "Novo projeto", tasks: [] }; 
+Certifique-se de enviar tanto o ID quanto o título do projeto no formato string
+com aspas duplas.
+*/
+server.post('/projects',contReq,(req,res)=>{
+  const {id, title} = req.body;
+  
   const project = {
     id,
     title,
-    tasks: []
+    task: []
   };
 
   projects.push(project);
-
-  return res.json(project);
+  
+  return res.json(projects);
 });
 
-/**
- * Route params: id
- * Request body: title
- * Altera o título do projeto com o id presente nos parâmetros da rota.
- */
-server.put('/projects/:id', checkProjectExists, (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
-
-  const project = projects.find(p => p.id == id);
-
-  project.title = title;
-
-  return res.json(project);
+/* ROTA PARA LISTAR OS PROJETOS E TAREFAS
+GET /projects: Rota que lista todos projetos e suas tarefas;
+*/
+server.get('/projects',contReq,(req,res)=>{
+  return res.json(projects);
 });
 
-/**
- * Route params: id
- * Deleta o projeto associado ao id presente nos parâmetros da rota.
- */
-server.delete('/projects/:id', checkProjectExists, (req, res) => {
-  const { id } = req.params;
+/* ROTA PARA ALTERAR UM PROJETO
+PUT /projects/:id: A rota deve alterar apenas o título do projeto com o id 
+presente nos parâmetros da rota;
+*/
+server.put('/projects/:id',checkID,contReq,(req,res)=>{
+  const {id} = req.params;
+  const {title} = req.body;
 
-  const projectIndex = projects.findIndex(p => p.id == id);
+  for (var i = 0; i < projects.length; i++){
+    if (projects[i].id == id){
+      projects[i].title = title;
+    }
+  }
 
-  projects.splice(projectIndex, 1);
+  return res.json(projects);
+});
+
+/* ROTA PARA DELETAR UM PROJETO
+DELETE /projects/:id: A rota deve deletar o projeto com o id presente nos 
+parâmetros da rota;
+*/
+server.delete('/projects/:id',checkID,contReq,(req,res)=>{
+  const {id} = req.params;
+
+  for (var i = 0; i < projects.length; i++){
+    if (projects[i].id == id){
+      projects.splice(i,1);
+    }
+  }
 
   return res.send();
 });
 
-/**
- * Route params: id;
- * Adiciona uma nova tarefa no projeto escolhido via id; 
- */
-server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
+/* ROTA PARA ADICIONAR TAREFA EM UM PROJETO
+POST /projects/:id/tasks: A rota deve receber um campo title e armazenar uma 
+nova tarefa no array de tarefas de um projeto específico escolhido através do id
+presente nos parâmetros da rota;
+*/
+server.post('/projects/:id/tasks',checkID,contReq,(req,res)=>{
+  const {id} = req.params;
+  const {title} = req.body;
 
-  const project = projects.find(p => p.id == id);
+  for (var i = 0; i < projects.length; i++){
+    if (projects[i].id == id){
+      projects[i].task = title;
+    }
+  }
 
-  project.tasks.push(title);
-
-  return res.json(project);
+  return res.json(projects);
 });
 
-server.listen(4000);
+server.listen(3000);
